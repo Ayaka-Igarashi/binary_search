faea08dce3ac662c2b39990eceee96d088966d87
Ayaka Igarashi <igarashi.a.ab@m.titech.ac.jp>
2018-06-20 11:14:44 +0900

    kadai4-1

--- src/works.c
+++ src/works.c
@@ -4,6 +4,25 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+    int zikan=0;
+    int hito=0;
+    int i2=0;
+    
+    while(hito<k){
+    while(zikan<m){
+    
+        zikan+=A[i2];
+        i2+=1;
+        
+        if(i2==n){return 1;}
+    }
+    hito+=1;
+    zikan=0;
+    }
+    if(hito<n-1){return 0;}
+}
+
 
 int main(){
   int i, lb, ub;
@@ -12,6 +31,40 @@ int main(){
     scanf("%d", &A[i]);
   }
 
+    lb=0;
+    ub=1000000000;
+    
+    while(ub-lb>1){
+        int m= (lb+ub)/2;
+    //
+        int zikan=0;
+        int hito=0;
+        int i2=0;
+        int a=0;
+        
+        while(hito<k&&i2<n){
+            while(zikan<m){
+                
+                zikan+=A[i2];
+                i2+=1;
+                
+                if(i2==n)a=1; break;
+            }
+            hito+=1;
+            zikan=0;
+        }
+        if(hito<n-1)a=0;
+     //
+        
+        if(a==0){
+            lb=m;
+        }else{
+            ub=m;
+        }
+    
+    }
+    
+    printf("%d\n",lb);
 
   return 0;
 }
